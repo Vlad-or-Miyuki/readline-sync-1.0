48c82cdf0ef2b0ee843e895fdb2a7361713be6e6
Miyuki <7001vlbl@bk.ru>
2023-10-24 10:52:32 +0300

    задание 4 фикс

--- task4.js
+++ task4.js
@@ -5,7 +5,7 @@ const height = readlineSync.question('Введите высоту')
 
 const calculateRectangleProperties = (width, height) => {
   var P = (2 * width) + (2 * height);
-  var S = (width**2 + height**2);
-  return [P,S]
+  var S = (width * height);
+  return [P, S]
 }
 console.log(calculateRectangleProperties(width, height));
