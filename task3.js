1072c7e286fd054a73b58ad21b5ef521420e060d
Miyuki <7001vlbl@bk.ru>
2023-10-24 09:56:06 +0300

    Задание 3

--- task3.js
+++ task3.js
@@ -4,9 +4,8 @@ const price = readlineSync.question('Введите цену товара: ');
 const discountPercentage = readlineSync.question('Введите скидку: ');
 
 const calculateDiscount = (price, discountPercentage) => {
-  const result = price * discountPercentage
+  const discount = price * discountPercentage / 100;
+  const result = price - discount;
   return result
-
 };
-
-console.log(calculateDiscount( ))
+console.log(calculateDiscount(price, discountPercentage))
